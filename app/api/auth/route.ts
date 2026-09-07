b6fedb13c16d446e740a5662f4ea888c2f62671a
Ravindu-Shehan <ravindushehan200122@gmail.com>
2026-09-07 17:47:52 +0530

    add bcryptjs for password hashing and implement user authentication route✏️

---
+++ app/api/auth/route.ts
@@ -0,0 +1,60 @@
+import prisma from "@/lib/prisma";
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import {compare} from "bcryptjs";
+
+export async function POST(request: NextRequest) {
+
+  const body = await request.json();
+
+  console.log(body);
+
+  if(body.email == null) {
+
+    return NextResponse.json(
+      {
+        message: "Email is required",
+      }
+    )
+  }
+
+  const user = await prisma.user.findFirst(
+    {
+    where: {
+      email: body.email,
+    },
+  });
+  
+
+  console.log(user);
+
+  if(user == null) {
+
+    return NextResponse.json(
+      {
+        message: "User not found",
+      }
+    )
+  }
+
+  const isPasswordValid = await compare(body.password, user.password);
+
+  if(isPasswordValid) {
+
+    return NextResponse.json(
+      {
+        message: "Login successful",
+      }
+    )
+
+}else {
+
+    return NextResponse.json(
+      {
+        message: "Invalid password",
+      }
+    )
+  }
+
+
+}
